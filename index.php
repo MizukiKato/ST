4277752679aac7c43847562d3d73c8a0f44869ab
Mizuki Kato <kato@sindbad-int.com>
2013-11-13 22:19:00 +0900

    kato want to go home

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html xml:lang="ja" lang="ja" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:mixi="http://mixi-platform.com/ns#">
+<head>
+<meta charset="UTF-8">
+<meta name="format-detection" content="telephone=no">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+<meta property="og:title" content="【中学受験】学校別・過去問解説映像授業-スタディ・タウン-" />
+<meta property="og:description" content="首都圏の中学受験・算数と理科の過去問解説授業です。中学受験専門・家庭教師の一橋セイシン会の有名家庭教師が、映像で、1校ごとに丁寧に過去問の解説授業をします。" />
+<meta property="og:url" content="http://chugakujuken.studytown.jp/sp/" />
+<meta property="og:image" content="http://chugakujuken.studytown.jp/img/common/ogp-common.png" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="【中学受験】学校別・過去問解説映像授業-スタディ・タウン-" />
+<meta property="fb:app_id" content="457890110913382" />
+<meta property="fb:admins" content="100003065077004" />
+<meta property="og:auther" content="Takeru Nakanishi" />
+<meta property="og:locale" content="ja_JP" />
+<link href="https://plus.google.com/106641171177042189508" rel="publisher" />
+<title>【中学受験】学校別・過去問解説映像授業-スタディ・タウン-</title>
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/head.php"); ?>
+</head>
+
+<body id="coupon">
+<!-- container -->
+<div id="container">
+
+	<!-- header start -->
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/header.php"); ?>
+	<!-- header end -->
+
+	<!-- main -->
+	<div id="main" class="cf">
+		<h2>1教科 無料利用クーポンのご案内</h2>
+
+		<!-- topic-path -->
+		<div id="topic-path" class="cf">
+			<ul>
+				<li><a href="/sp/">HOME</a>&nbsp;≫&nbsp;</li>
+				<li>1教科 無料利用クーポンのご案内</li>
+			</ul>
+		</div>
+		<!-- topic-path -->
+
+		<section class="intro">
+            <h3><img src="/sp/img/coupon/ti_coupon_h2.png" alt="通常1教科14,800円が、いまなら無料で視聴できます！"></h3>
+            <p>家庭教師の一橋セイシン会による映像授業サービス『スタディ・タウン中学受験』の無料利用のご案内です。</p>
+		</section>
+        <section class="sect01 cf">
+            <h3>過去問が、本から映像授業へ！</h3>
+            <img src="/sp/img/coupon/img_sect01.jpg" alt="">
+        </section>
+        <section class="sect02">
+            <h3>HP上で以下の番号をご入力いただくと、<br />映像授業(1教科)が無料になります</h3>
+            <div class="inner">
+                <div class="box">
+                    <h4>無料クーポン番号</h4>
+                    <p class="coupon-number">663036216</p>
+                    <ul>
+                        <li>※上記クーポン番号の有効期間は、<span class="red">2013年1月26日(土)</span>です。</li>
+                        <li>※映像授業1教科3年分<span class="uchikeshi"><span class="black">14,800円</span></span> → <span class="red">無料</span>になります。</li>
+                        <li>※ご友人の方に上記の番号をご紹介いただいても構いません。</li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <h4>クーポンのご利用方法</h4>
+                    <div class="img"><img src="/sp/img/coupon/img_riyouhou.png" alt="ご利用方法"></div>
+                    <div class="txt">商品選択画面で、無料クーポン番号をご入力ください。</div>
+                </div>
+            </div>
+        </section>
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/contact.php"); ?>
+
+	</div>
+	<!-- main -->
+
+	<!-- pagetop -->
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/pagetop.php"); ?>
+	<!-- pagetop -->
+	
+</div>
+<!-- container  -->
+
+<!-- nav start -->
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/bottom-menu.php"); ?>
+<!-- nav end -->
+
+<!-- footer start -->
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/sp/inc/footer.php"); ?>
+<!-- footer end -->
+
+</body>
+</html>
